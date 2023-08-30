f04e00c410c940703c0714e7d3cb7d1fc814eccf
Kevin Buffardi <kbuffardi@csuchico.edu>
2023-08-30 11:35:09 -0700

    Remove using std

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <cmath>
 
-using std::cin;
-using std::cout;
-using std::endl;
-
 int main()
 {
     cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " <<x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " <<x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    std::cin >> x >> y;
+    std::cout << "Addition: " << x + y << std::endl;
+    std::cout << "Subtraction: " << x - y << std::endl;
+    std::cout << "Multiplication: " <<x * y << std::endl;
+    std::cout << "Division: " << x / y << std::endl;
+    std::cout << "Remainder: " <<x % y << std::endl;
+    std::cout << "Square Root: " << sqrt(x) << std::endl;
+    std::cout << "Square: " << pow(x, y) << std::endl;
 
     return 0;
 }
