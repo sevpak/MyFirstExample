65bf522c1fdc8d2602f53d45b21f06f1e1197c82
Sean Evpak <sevpak@csuchico.edu>
2025-09-08 11:35:38 -0700

    Changed teh output format to display full math operations. Closes #62

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@ int main()
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    cout << x << "+" << y << "=" << x + y << endl;
+    cout << x << "-" << y << "=" << x - y << endl;
+    cout << x << "*" << y << "="  << x * y << endl;
+    cout << x << "/" << y << "="  << x / y;
+    cout << "with remainder of " << x % y << endl;
+    cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout << x << "^" << y << "=" << pow(x, y) << endl;
 
     return 0;
 }
